6b8d9ec58bcc508ece8e89e82d8937ebdd180982
박대규 <mamma1234@gmail.com>
2019-11-13 19:58:31 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+// core components
+import Admin from "layouts/Admin.js";
+import RTL from "layouts/RTL.js";
+
+import "assets/css/material-dashboard-react.css?v=1.8.0";
+
+import { Provider } from 'mobx-react';
+import store from './store';
+import * as serviceWorker from './serviceWorker';
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Provider store={store}>
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/rtl" component={RTL} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>
+  </Provider>,
+  document.getElementById("root")
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
